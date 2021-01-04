04c7ad6a290d275f042e5b51381060d55a291fa9
BillMC98 <billmakridis2@gmail.com>
2021-01-04 19:59:30 +0200

    Finaly main

---
+++ RSA/main.cpp
@@ -0,0 +1,18 @@
+#include "RSA.h"
+
+int main(void){
+  // Create two file handles
+  std::ifstream inp;
+  std::ofstream out;
+
+  srand (time(NULL));
+  RSA rsa;
+
+  // encryption stage
+  rsa.Encryption(inp,"plain.txt", out, "cipher.txt");
+
+  // decryption stage
+  rsa.Decryption(inp, "cipher.txt", out, "decipher.txt");
+
+  return 0;
+}
