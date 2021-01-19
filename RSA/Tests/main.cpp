15d52ef3b5a994899fa9dcb1681f2a352f0f2c29
Emmanouilidis Konstantinos <emmanouilidis.kons@gmail.com>
2021-01-19 18:47:42 +0200

    Update main.cpp

--- RSA/Tests/main.cpp
+++ RSA/Tests/main.cpp
@@ -4,7 +4,7 @@ int main(void){
   srand (time(NULL));
   
   //testing for random plain text files
-  int max_iter = 10;
+  int max_iter = 1000;
   std::cout << "Start testing..." << "\n";
   std::cout << "Number of correct results: " << testing(max_iter) << " out of " << max_iter;
 
